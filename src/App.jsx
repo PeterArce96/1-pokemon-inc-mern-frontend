a2dacb7f24d183a3b904017fdc25597025908d32
PeterArce96 <parce0496@gmail.com>
2022-05-25 21:07:32 -0500

    creación de layout Pokemon y route en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Pokemons from './layouts/Pokemons';
 
 function App() {
   return (
     <Routes>
-      <Route path='/create' element = {<h2>Crear Pokemons</h2>}/>
-      <Route path='/update' element = {<h2>Update Pokemons</h2>}/>
-      <Route path='/' element = {<h2>Pokemons</h2>}/>
-      <Route path='*' element = {<h2>404</h2>}/>
+      <Route path='/' element={<Pokemons/>}>
+        <Route path='create' element = {<h2>Crear Pokemons</h2>}/>
+        <Route path='update' element = {<h2>Update Pokemons</h2>}/>
+        <Route index element = {<h2>Pokemons</h2>}/>
+        <Route path='*' element = {<h2>404</h2>}/>
+      </Route>
     </Routes>
   );
 }
