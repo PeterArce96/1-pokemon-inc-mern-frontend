61c9a6bd57733eb46232a761f549118505859eb6
PeterArce96 <parce0496@gmail.com>
2022-05-26 22:11:45 -0500

    PokemonsForm.jsx estilos

--- src/layouts/PokemonsLayout.jsx
+++ src/layouts/PokemonsLayout.jsx
@@ -1,11 +1,8 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/sections/Header";
-import usePokemons from "../hooks/usePokemons";
 
 
 const PokemonsLayout = () => {
-    const context = usePokemons();
-    console.log(context);
 
     const applicationName = "PokémonInc";
 
--- src/pages/pokemons/PokemonsCreate.jsx
+++ src/pages/pokemons/PokemonsCreate.jsx
@@ -5,6 +5,7 @@ const PokemonsCreate = () => {
         <section className="py-8">
             <div className="flex flex-col gap-8 container mx-auto px-8 md:px-4">
                 <h2 className="text-yellow-400 text-3xl text-center font-bold">Crear Pokemon</h2>
+                <PokemonsForm/>
             </div>
         </section>
     );
