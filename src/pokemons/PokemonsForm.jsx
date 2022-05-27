6968f10b985d871b623faf30e9262c3908e76110
PeterArce96 <parce0496@gmail.com>
2022-05-26 22:35:55 -0500

    yup en Pokemonsform, requerimientos, divs en form

--- src/pokemons/PokemonsForm.jsx
+++ src/pokemons/PokemonsForm.jsx
@@ -1,5 +1,5 @@
-import { Field, Form, Formik } from "formik";
-
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as yup from 'yup';
 
 const PokemonsForm = () => {
     return (
@@ -12,6 +12,13 @@ const PokemonsForm = () => {
                 special: '',
                 image: {}
             }}
+            validationSchema = {yup.object({
+                name: yup.string().required('El nombre es requerido'),
+                type: yup.string().required('El tipo es requerido'),
+                hp: yup.number().required('El HP es requerido').positive().integer(),
+                attack: yup.string().required('El ataque es requerido'),
+                special: yup.string().required('El especial es requerido'),
+            })}
             onSubmit={(values) => {
                 console.log(values);
             }}
@@ -21,11 +28,31 @@ const PokemonsForm = () => {
                     <Form
                         onSubmit={handleSubmit} className="flex flex-col gap-4 bg-zinc-700 rounded-md w-4/5 max-w-sm mx-auto p-4"
                     >
-                        <Field name='name' type="text" placeholder='Nombre' className="bg-zinc-800 rounded p-2"/>   
-                        <Field name='type' type="text" placeholder='Tipo' className="bg-zinc-800 rounded p-2"/>   
-                        <Field name='hp' type="number" placeholder='HP' className="bg-zinc-800 rounded p-2"/>   
-                        <Field name='attack' type="text" placeholder='Ataque' className="bg-zinc-800 rounded p-2"/>   
-                        <Field name='special' type="text" placeholder='Especial' className="bg-zinc-800 rounded p-2"/>
+                        <div className="flex flex-col gap-1">
+                            <Field name='name' type="text" placeholder='Nombre' className="bg-zinc-800 rounded p-2"/>
+                            <ErrorMessage name="name" component="span" className="text-rose-500 text-sm"/>
+                        </div>
+
+                        <div className="flex flex-col gap-1">
+                            <Field name='type' type="text" placeholder='Tipo' className="bg-zinc-800 rounded p-2"/>
+                            <ErrorMessage name="type" component="span" className="text-rose-500 text-sm"/>
+                        </div>
+
+                        <div className="flex flex-col gap-1">
+                            <Field name='hp' type="number" placeholder='HP' className="bg-zinc-800 rounded p-2"/>
+                            <ErrorMessage name="hp" component="span" className="text-rose-500 text-sm"/>
+                        </div> 
+
+                        <div className="flex flex-col gap-1">
+                            <Field name='attack' type="text" placeholder='Ataque' className="bg-zinc-800 rounded p-2"/>
+                            <ErrorMessage name="attack" component="span" className="text-rose-500 text-sm"/>
+                        </div>
+
+                        <div className="flex flex-col gap-1">
+                            <Field name='special' type="text" placeholder='Especial' className="bg-zinc-800 rounded p-2"/>
+                            <ErrorMessage name="special" component="span" className="text-rose-500 text-sm"/>
+                        </div>
+                        
                         <input type="file" className="
                             bg-zinc-800
                             rounded p-2 text-sm cursor-pointer 
