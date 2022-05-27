fa108f483e58425870e964aa8afc84e5b2b415aa
PeterArce96 <parce0496@gmail.com>
2022-05-26 20:05:09 -0500

    PokemonsContext.jsx PokemonsProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import {Routes, Route, Navigate} from 'react-router-dom';
 
 import './App.css';
 import PokemonsLayout from './layouts/pokemons/PokemonsLayout';
+import { PokemonsProvider } from './context/PokemonsContext';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Navigate to="/pokemons" replace={true}/>}/>
-      <Route path="/pokemons" element={<PokemonsLayout />}>
-        <Route path="create" element={<h2>Create Pokémons</h2>} />
-        <Route path="update" element={<h2>Update Pokémons</h2>} />
-        <Route index element={<h2>Pokémons</h2>} />
-      </Route>
-      <Route path="*" element={<h2>404</h2>} />
-    </Routes>
+    <PokemonsProvider>
+      <Routes>
+        <Route path='/' element={<Navigate to="/pokemons" replace={true}/>}/>
+        <Route path="/pokemons" element={<PokemonsLayout />}>
+          <Route path="create" element={<h2>Create Pokémons</h2>} />
+          <Route path="update" element={<h2>Update Pokémons</h2>} />
+          <Route index element={<h2>Pokémons</h2>} />
+        </Route>
+        <Route path="*" element={<h2>404</h2>} />
+      </Routes>
+    </PokemonsProvider>
   );
 }
 
